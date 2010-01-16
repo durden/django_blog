7121bd1962d2b4aaaaec0605a9d8ffd1a9157141
Luke Lee <durdenmisc@gmail.com>
2010-01-16 11:27:44 -0600

    Adding admin/auth/sessions/contenttypes apps and setup admin to autodiscover

--- urls.py
+++ urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
-from blog_template.views import *
+# Admin
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from blog_template.views import *
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 
     # Homepage is base template with nothing else (i.e. index page)
     (r'^$', static_page, {'template': 'base'}),
