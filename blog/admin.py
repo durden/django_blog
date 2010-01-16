2cd058e7e536b76966d080ffae4e722c3a614c46
Luke Lee <durdenmisc@gmail.com>
2010-01-16 11:42:21 -0600

    Add published to the post listings and prepopulate slug with title

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,8 @@ from blog_template.blog.models import *
 
 class PostAdmin(admin.ModelAdmin):
     filter_horizontal = ('categories',)
+    list_display = ('title', 'published')
+    prepopulated_fields = {'slug' : ('title',)}
 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
