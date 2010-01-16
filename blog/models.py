583db456258a7ae4e4efecb58d4fa26954f85444
Luke Lee <durdenmisc@gmail.com>
2010-01-16 11:21:59 -0600

    Added base post and category models, added sqlite support to settings

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120, unique=True)
+    body = models.TextField()
+    published = models.DateTimeField(default=datetime.now)
+    categories = models.ManyToManyField(Category)
--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'db.sqlite')
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
