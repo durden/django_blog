b52600b21418f11d43270d096f82242258ec2b47
Luke Lee <durdenmisc@gmail.com>
2010-01-16 11:38:40 -0600

    Cleanup selection of categories when creating new posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from blog_template.blog.models import *
 
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
