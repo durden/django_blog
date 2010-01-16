64bd1dd517501f9eaca223f9700f1744e27ed442
Luke Lee <durdenmisc@gmail.com>
2010-01-16 11:35:07 -0600

    Added pretty names to models so admin can show them

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,15 @@ from datetime import datetime
 class Category(models.Model):
     name = models.CharField(max_length=60)
 
+    def __unicode__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(max_length=120, unique=True)
     body = models.TextField()
     published = models.DateTimeField(default=datetime.now)
     categories = models.ManyToManyField(Category)
+
+    def __unicode__(self):
+        return self.title
