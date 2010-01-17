132c46059bbe19e0e2f7fa597a5335123d6d07fa
Luke Lee <durdenmisc@gmail.com>
2010-01-17 10:39:53 -0600

    Changing urls to use static_page function to append .html to templates, renamed about template to match

--- urls.py
+++ urls.py
@@ -19,9 +19,8 @@ urlpatterns = patterns('',
 
     # Grab any word, and pass it as 'template' to static_page view
     # Not using b/c it appends .html for templates
-    #(r'^(?P<template>\w+)/$', static_page),
+    url(r'^(?P<template>\w+)/$', static_page, name='static_page'),
 
-    url(r'^(?P<template>\w+)/$', direct_to_template, name='static_page'),
     (r'^static/(.*)$', 'django.views.static.serve',
         {'document_root': 'static/'}),
 )
