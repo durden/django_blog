1ebee0d74f182bd48f8edcacfcabe6b9d64e6307
Luke Lee <durdenmisc@gmail.com>
2010-01-21 19:22:07 -0600

    Removing un-used imports and duplicate url for blog.urls

--- urls.py
+++ urls.py
@@ -2,12 +2,7 @@
 from django.conf.urls.defaults import *
 
 # Views
-from django.views.generic.simple import direct_to_template
-from django.views.generic import list_detail
 from blog_template.views import *
-
-# Models
-from blog_template.blog.models import Post
 
 # Admin
 from django.contrib import admin
@@ -20,16 +15,13 @@ urlpatterns = patterns('',
 
     (r'^admin/(.*)', admin.site.root),
 
-    # Homepage is base template with nothing else (i.e. index page)
-    (r'^$', include('blog_template.blog.urls')),
+    (r'^static/(.*)$', 'django.views.static.serve',
+        {'document_root': 'static/'}),
 
-    # Blogs
-    (r'^blog/$', include('blog_template.blog.urls')),
+    # Homepage is base template with nothing else (i.e. index page)
+    (r'^', include('blog_template.blog.urls')),
 
     # Grab any word, and pass it as 'template' to static_page view
     # Not using b/c it appends .html for templates
     url(r'^(?P<template>\w+)/$', static_page, name='static_page'),
-
-    (r'^static/(.*)$', 'django.views.static.serve',
-        {'document_root': 'static/'}),
 )
