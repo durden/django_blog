9f1009e5346c75cd63ff829a5cf8dee959a61955
Luke Lee <durdenmisc@gmail.com>
2010-01-30 16:24:19 -0600

    Added slugs for pretty urls instead of blog ids, cleaned up comment count to work for post and detail templates

--- blog/urls.py
+++ blog/urls.py
@@ -8,10 +8,11 @@ from django.views.generic import list_detail
 from blog_template.blog.models import Post
 
 urlpatterns = patterns('',
-    url(r'post/(?P<object_id>\d+)/$',
+    url(r'post/(?P<slug>[a-z-]+)/$',
             list_detail.object_detail,
                 {'queryset': Post.objects.all(),
-                 'template_object_name': 'post',}, name='single_post'),
+                 'template_object_name': 'post',
+                 'slug_field' : 'slug',}, name='single_post'),
 
     # Homepage is base template with nothing else (i.e. index page)
     url(r'^$', list_detail.object_list,
