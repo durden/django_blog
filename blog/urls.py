a8e5eed9d9c853eeb5c9d399b743a06de0f61c5b
Luke Lee <durdenmisc@gmail.com>
2010-01-31 11:12:06 -0600

    Added archive listings organized by month

--- blog/urls.py
+++ blog/urls.py
@@ -2,18 +2,25 @@
 from django.conf.urls.defaults import *
 
 # Views
-from django.views.generic import list_detail
+from django.views.generic import list_detail, date_based
 from blog.views import *
 
 # Models
 from blog.models import Post
 
 urlpatterns = patterns('',
+    # Post detail
     url(r'post/(?P<slug>[a-z-]+)/$', blog_generic_view,
             {'redirect_to': list_detail.object_detail, 'slug_field' : 'slug',},
         name='single_post'),
 
-    # Homepage is base template with nothing else (i.e. index page)
+    # Archives
+    url(r'^archive/(?P<month>[a-z]+)/(?P<year>\d{4})/$', blog_generic_view,
+            {'redirect_to': date_based.archive_month, 'date_field': 'published',
+                'template_name': 'blog/post_list.html',}),
+
+    # Home page (post list)
     url(r'^$', blog_generic_view, {'redirect_to': list_detail.object_list},
         name="blog_home"),
+
 )
